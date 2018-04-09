b9bc3b287a773220ca16d7287d910e672b6310b0
mengbishu <841616394@qq.com>
2018-04-09 15:40:57 +0800

    1

--- main.ts
+++ main.ts
@@ -314,4 +314,14 @@ namespace pixel {
     //% blockId="neopixel_colors" block="lleds"
     declare function lleds(leds: string, interval?: number): void;
 
+    /**
+     * Scroll a number on the screen. If the number fits on the screen (i.e. is a single digit), do not scroll.
+     * @param interval speed of scroll; eg: 150, 100, 200, -100
+     */
+    //% help=basic/show-number
+    //% weight=96
+    //% blockId=device_show_number block="show|number %number" blockGap=8
+    //% async
+    //% parts="ledmatrix" interval.defl=150 shim=basic::showNumber
+    declare function showNumber(value: number, interval?: number): void;    
 }
