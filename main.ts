51caef94d3a3be1c2f813c979789fc9347a78e06
mengbishu <841616394@qq.com>
2018-04-10 15:39:54 +0800

    1

--- main.ts
+++ main.ts
@@ -39,7 +39,7 @@ enum NeoPixelMode {
 /**
  * Functions to operate NeoPixel strips.
  */
-//% color=#0078D7 weight=100 icon="\uf00a"
+//% color=#0078D7  icon="\uf00a"
 namespace pixel {
     /**
      * A NeoPixel strip
