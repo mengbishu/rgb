90028d47203a2e551a70ab89f7a5f27383a09f7b
mengbishu <841616394@qq.com>
2018-04-10 16:31:28 +0800

    1

--- rgb.cpp
+++ rgb.cpp
@@ -6,15 +6,19 @@
  */
 //% color=#0078D7 icon="\uf00a"
 namespace rgb {
-    /**
-     * Scroll a number on the screen. If the number fits on the screen (i.e. is a single digit), do not scroll.
-     * @param interval speed of scroll; eg: 150, 100, 200, -100
-     */
+    //% help=basic/show-leds
+    //% weight=95 blockGap=8
+    //% imageLiteral=1 async
+    //% blockId=device_show_leds
+    //% block="show leds" icon="\uf00a"
+    void lleds(ImageLiteral leds, int interval = 400) {
+      uBit.display.print(MicroBitImage(imageBytes(leds)), 0, 0, 0, interval);
+    }
+
     //% help=basic/show-number
     //% weight=96
     //% blockId=device_show_number block="show|number %number" blockGap=8
     //% async
-    //% parts="ledmatrix"
     void showN(int value, int interval = 150) {
       if (interval <= 0)
         interval = 1;
@@ -26,25 +30,6 @@ namespace rgb {
       }
     }
 
-
-
-    /**
-     * Draws an image on the LED screen.
-     * @param leds the pattern of LED to turn on/off
-     * @param interval time in milliseconds to pause after drawing
-     */
-    //% help=basic/show-leds
-    //% weight=95 blockGap=8
-    //% imageLiteral=1 async
-    //% blockId=device_show_leds
-    //% block="show leds" icon="\uf00a"
-    void lleds(ImageLiteral leds, int interval = 400) {
-      uBit.display.print(MicroBitImage(imageBytes(leds)), 0, 0, 0, interval);
-    }
-
-    /**
-     * Creates an image that fits on the LED screen.
-     */
     //% weight=75 help=images/create-image
     //% blockId=device_build_image block="create image"
     //% parts="ledmatrix"
@@ -52,9 +37,6 @@ namespace rgb {
       return;
     }
 
-    /**
-     * Creates an image with 2 frames.
-     */
     //% weight=74 help=images/create-big-image
     //% blockId=device_build_big_image block="create big image" imageLiteral=2
     //% parts="ledmatrix"
--- rgb.d.ts
+++ rgb.d.ts
@@ -1,45 +1,33 @@
-    /**
-     * Provides access to basic micro:bit functionality.
-     */
-    //% color=#0078D7 icon="\uf00a"
+
+//% color=#0078D7 icon="\uf00a"
 declare namespace rgb {
-    /**
-     * Draws an image on the LED screen.
-     * @param leds the pattern of LED to turn on/off
-     * @param interval time in milliseconds to pause after drawing
-     */
+
     //% help=basic/show-leds
     //% weight=95 blockGap=8
     //% imageLiteral=1 async
     //% blockId=device_show_leds
     //% block="show leds" icon="\uf00a"
-    //% interval.defl=400 shim=rgb::lleds
+    //% interval.defl=400 shim = rgb:: lleds
     function lleds(leds: string, interval?: number): void;
 
-    /**
-     * Scroll a number on the screen. If the number fits on the screen (i.e. is a single digit), do not scroll.
-     * @param interval speed of scroll; eg: 150, 100, 200, -100
-     */
+
     //% help=basic/show-number
     //% weight=96
     //% blockId=device_show_number block="show|number %number" blockGap=8
     //% async
-    //% interval.defl=150 shim=rgb::showN  
+    //% interval.defl=150 shim = rgb:: showN  
     function showN(value: number, interval?: number): void;
 
-    /**
-     * Creates an image that fits on the LED screen.
-     */
     //% weight=75 help=images/create-image
     //% blockId=device_build_image block="create image"
-    //% imageLiteral=1 shim=rgb::createI
+    //% imageLiteral=1 
+    //% shim = rgb:: createI
+    //% parts="ledmatrix" 
     function createI(leds: string): void;
 
-    /**
-     * Creates an image with 2 frames.
-     */
     //% weight=74 help=images/create-big-image
     //% blockId=device_build_big_image block="create big image" imageLiteral=2
-    //% parts="ledmatrix" shim=rgb::createBigI
+    //% shim = rgb:: createBigI
+    //% parts="ledmatrix" 
     function createBigI(leds: string): Image;
 }
