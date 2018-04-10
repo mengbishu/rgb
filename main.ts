2711765b6acc4562b196a2cc1fc84522702c1bed
mengbishu <841616394@qq.com>
2018-04-10 11:05:28 +0800

    1

--- main.ts
+++ main.ts
@@ -1,4 +1,51 @@
 /**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=#0078D7 weight=100 icon="\uf00a"
+declare namespace rgb {
+    /**
+     * Draws an image on the LED screen.
+     * @param leds the pattern of LED to turn on/off
+     * @param interval time in milliseconds to pause after drawing
+     */
+    //% help=basic/show-leds
+    //% weight=95 blockGap=8
+    //% imageLiteral=1 async
+    //% blockId=device_show_leds
+    //% block="show leds" icon="\uf00a"
+    //% parts="ledmatrix" interval.defl=400 shim=rgb::lleds
+    function lleds(leds: string, interval?: number): void;
+
+    /**
+     * Scroll a number on the screen. If the number fits on the screen (i.e. is a single digit), do not scroll.
+     * @param interval speed of scroll; eg: 150, 100, 200, -100
+     */
+    //% help=basic/show-number
+    //% weight=96
+    //% blockId=device_show_number block="show|number %number" blockGap=8
+    //% async
+    //% parts="ledmatrix" interval.defl=150 shim=rgb::showN  
+    function showN(value: number, interval?: number): void;
+
+    /**
+     * Creates an image that fits on the LED screen.
+     */
+    //% weight=75 help=images/create-image
+    //% blockId=device_build_image block="create image"
+    //% parts="ledmatrix" imageLiteral=1 shim=rgb::createI
+    function createI(leds: string): Image;
+
+    /**
+     * Creates an image with 2 frames.
+     */
+    //% weight=74 help=images/create-big-image
+    //% blockId=device_build_big_image block="create big image" imageLiteral=2
+    //% parts="ledmatrix" shim=rgb::createBigI
+    function createBigI(leds: string): Image;
+}
+
+
+/**
  * Well known colors for a NeoPixel strip
  */
 enum NeoPixelColors {
