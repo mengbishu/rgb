550637116b8330f87075191d7bf8f0240e182fbc
mengbishu <841616394@qq.com>
2018-04-11 14:35:54 +0800

    1

--- main.ts
+++ main.ts
@@ -44,7 +44,8 @@ namespace pixel {
     /**
      * A NeoPixel strip
      */
-
+    let buf: string[] = ['a', 'b', 'c'];
+    let chrs: string[] = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     export class Strip {
         buf: Buffer;
         pin: DigitalPin;
@@ -55,7 +56,7 @@ namespace pixel {
         _mode: NeoPixelMode;
         _matrixWidth: number; // number of leds in a matrix - if any
 
-        chrs: string[] = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+        
         
 
 
