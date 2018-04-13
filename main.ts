62a6c70d5fd3f89302fb780fa63e9d3e83e24928
mengbishu <841616394@qq.com>
2018-04-13 11:05:42 +0800

    1

--- main.ts
+++ main.ts
@@ -186,7 +186,7 @@ namespace pixel {
                     for (i = 0; i < 8; i++) {
                         serial.writeLine("");
                         for (j = 0; j < 8; j++){
-                            screen[j] = (screen[j] << i) | (queue[k * 8 + j] & (0x1 << (7-i)) >> (7-i));
+                            screen[j] = (screen[j] << i) | (queue[k * 8 + j] & (0x1 << (7-i))) >> (7-i);
                             serial.writeNumber((queue[k * 8 + j] & (0x1 << (7-i))) >> (7-i));
                             serial.writeLine("");
                         }
