0323a4e2eb56074995da6f58f3f7a1866a954c58
mengbishu <841616394@qq.com>
2018-05-02 13:41:29 +0800

    1

--- main.ts
+++ main.ts
@@ -738,47 +738,4 @@ namespace Matrix {
         return b;
     }
 
-    //% weight=50 blockGap=8
-    //% help=images/icon-image
-    //% blockId=builtin_image block="icon image %i"
-    //% i.fieldEditor="gridpicker"
-    //% i.fieldOptions.width="400" i.fieldOptions.columns="5"
-    //% i.fieldOptions.itemColour="black" i.fieldOptions.tooltips="true"
-    export function myicon(i: Pic): Image {
-        switch (i) {
-            case Pic.smile: return images.createImage(`
-                                        . # . # .
-                                        # # # # #
-                                        # # # # #
-                                        . # # # .
-                                        . . # . .`);
-
-            case Pic.eagleEye: return images.createImage(`
-                                        . . . . .
-                                        . # . # .
-                                        . # # # .
-                                        . . # . .
-                                        . . . . .`);
-            //faces
-            case Pic.embarrassed: return images.createImage(`
-                                        . . . . .
-                                        . # . # .
-                                        . . . . .
-                                        # . . . #
-                                        . # # # .`);
-        }
-    }
-
-    //% weight=90 blockGap=8
-    //% blockId=basic_show_icon
-    //% block="show icon %i" icon="\uf00a"
-    //% parts="ledmatrix"
-    //% help=basic/show-icon
-    //% i.fieldEditor="gridpicker"
-    //% i.fieldOptions.width="400" i.fieldOptions.columns="5"
-    //% i.fieldOptions.itemColour="black" i.fieldOptions.tooltips="true"
-    export function showIcon(icon: Pic, interval = 600) {
-        let res = myicon(icon)
-        res.showImage(0, interval)
-    }
 }
