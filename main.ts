82926e2a4c6af5906cf6e5ceb168a151bc88058f
mengbishu <841616394@qq.com>
2018-05-02 13:47:16 +0800

    1

--- main.ts
+++ main.ts
@@ -746,24 +746,11 @@ namespace Matrix {
     //% i.fieldOptions.itemColour="black" i.fieldOptions.tooltips="true"
     export function myicon(i: Pic): number {
         switch (i) {
-            case Pic.smile: return 1;
-
+            case Pic.smile: return 1; 
             case Pic.eagleEye: return 2;
-            //faces
             case Pic.embarrassed: return 3;
         }
     }
 
-    //% weight=90 blockGap=8
-    //% blockId=basic_show_icon
-    //% block="show icon %i" icon="\uf00a"
-    //% parts="ledmatrix"
-    //% help=basic/show-icon
-    //% i.fieldEditor="gridpicker"
-    //% i.fieldOptions.width="400" i.fieldOptions.columns="5"
-    //% i.fieldOptions.itemColour="black" i.fieldOptions.tooltips="true"
-    export function showIcon(icon: Pic, interval = 600) {
-        let res = myicon(icon)
-        //res.showImage(0, interval)
-    }
+
 }
